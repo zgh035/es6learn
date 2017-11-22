b9c42d2ac930a9b348c2fc98f68e442c149ccb7d
郑高红 <gaohong.zheng@workbrother.com>
2017-11-22 17:45:24 +0800

    22日第一次提交

--- 11-22/11-22-1.js
+++ 11-22/11-22-1.js
@@ -1,6 +1,6 @@
 
 
-
+//对象和数组组合解构赋值
 let obj = {
     p : [
         "hello",
@@ -12,3 +12,27 @@ let obj = {
 
 let { p : [x , { y }]} = obj;
 console.log(x,y)
+
+
+let obj1 = {
+    a : [
+        2
+    ]
+}
+let {a : [ x1 ]} = obj1;
+console.log(x1)
+
+
+const node = {
+    a1 : {
+        b1 : {
+            c1 : {
+                d1 : 1,
+                d2 : 2
+            }
+        }
+    }
+};
+
+let { a1, a1 : { b1, b1 : { c1 , c1 : { d1,d2 } } } } = node;
+console.log(a1,b1,c1,d1,d2)
